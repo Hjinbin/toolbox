0649af57c732b8bd1e23ed3f81eab364a9b9c7b4
Hjinbin <1057316754@qq.com>
2020-06-11 17:38:10 +0800

    feat(upload-source-map-plugin): 初步完成自动上传sourcemap插件

--- packages/upload-source-map-plugin/src/utils/upload-file.js
+++ packages/upload-source-map-plugin/src/utils/upload-file.js
@@ -1,5 +1,29 @@
 const { request } = require('http')
+const fs = require('fs')
 
-module.exports = uploadFile = ({ url, filePath}) => {
-
+module.exports = uploadFile = ({ url, filePath, fileName }) => {
+  const match = url.match(/^(http:\/\/?)(.+)\:(\d+)(.*)/)
+  const [, , hostname, port, pathname] = match || []
+  if (!hostname) return
+  const options = {
+    host: hostname,
+    path: pathname + `?fileName=${fileName}`,
+    port,
+    method: 'POST',
+    headers: {
+      // 二进制文件没有特定或已知的 subtype，即使用 application/octet-stream
+      'Content-Type': 'application/octet-stream',
+      Connection: 'keep-alive',
+      // 数据以一系列分块的形式进行发送
+      'Transfer-Encoding': 'chunked'
+    }
+  }
+  const req = request(options)
+  fs.createReadStream(filePath)
+    .on('data', chunk => {
+      req.write(chunk)
+    })
+    .on('end', () => {
+      req.end()
+    })
 }
